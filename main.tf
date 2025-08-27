64e02cd3d5a2cb270e68485be5cdb51564009e6b
rosel <rosel@rosel>
2025-08-27 12:41:41 +0900

    initial

---
+++ main.tf
@@ -0,0 +1,4 @@
+module "my_role" {
+  source    = "./my_module"
+  role_name = var.role_name
+}
---
+++ output.tf
@@ -0,0 +1,6 @@
+output "role_name" {
+  value = module.my_role.role_name
+}
+output "role_arn" {
+  value = module.my_role.role_arn
+}
