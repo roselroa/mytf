dd2e6a9b7cac9d196224f71f79a6069f8531bb4b
rosel <rosel@rosel>
2025-08-27 13:08:56 +0900

    added comment on how to use local and github

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
-/*
+/* using local module
+module "my_role" {
+  source    = "./my_module"
+  role_name = var.role_name
+}
+*/
+
+/* using remote module from GitHub
 module "my_role" {
   source    = "github.com/roselroa/terraform-aws-myiamrole.git?ref=v1.0.0"
   role_name = var.role_name
 }
 */
 
+/* using remote module from Terraform Registry */
 module "my_role" {
   source  = "app.terraform.io/dulaanan/myiamrole/aws"
   version = ">=1.0.0"
