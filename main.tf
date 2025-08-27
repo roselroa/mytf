866fd8d65e2e6526f5f94da7f58869197c0e7704
rosel <rosel@rosel>
2025-08-27 13:05:56 +0900

    updated to use TF cloud

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
+/*
 module "my_role" {
-  source    = "./my_module"
+  source    = "github.com/roselroa/terraform-aws-myiamrole.git?ref=v1.0.0"
+  role_name = var.role_name
+}
+*/
+
+module "my_role" {
+  source  = "app.terraform.io/dulaanan/myiamrole/aws"
+  version = ">=1.0.0"
   role_name = var.role_name
 }
